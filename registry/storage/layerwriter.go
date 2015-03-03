15e6e80d28f8a18839079669715ed759428d43e7
David Lawrence <david.lawrence@docker.com>
2015-03-03 08:57:52 -0800

    Refactoring cloudfactory layer handler into a more generic storage middleware concept.
    This also breaks the dependency the storage package had on goamz
    Signed-off-by: David Lawrence <david.lawrence@docker.com> (github: endophage)

--- registry/storage/layerwriter.go
+++ registry/storage/layerwriter.go
@@ -14,9 +14,11 @@ import (
 	"github.com/docker/docker/pkg/tarsum"
 )
 
-// layerUploadController is used to control the various aspects of resumable
+var _ distribution.LayerUpload = &layerWriter{}
+
+// layerWriter is used to control the various aspects of resumable
 // layer upload. It implements the LayerUpload interface.
-type layerUploadController struct {
+type layerWriter struct {
 	layerStore *layerStore
 
 	uuid      string
@@ -27,65 +29,64 @@ type layerUploadController struct {
 	bufferedFileWriter
 }
 
-var _ distribution.LayerUpload = &layerUploadController{}
+var _ distribution.LayerUpload = &layerWriter{}
 
 // UUID returns the identifier for this upload.
-func (luc *layerUploadController) UUID() string {
-	return luc.uuid
+func (lw *layerWriter) UUID() string {
+	return lw.uuid
 }
 
-func (luc *layerUploadController) StartedAt() time.Time {
-	return luc.startedAt
+func (lw *layerWriter) StartedAt() time.Time {
+	return lw.startedAt
 }
 
 // Finish marks the upload as completed, returning a valid handle to the
 // uploaded layer. The final size and checksum are validated against the
 // contents of the uploaded layer. The checksum should be provided in the
 // format <algorithm>:<hex digest>.
-func (luc *layerUploadController) Finish(digest digest.Digest) (distribution.Layer, error) {
-	ctxu.GetLogger(luc.layerStore.repository.ctx).Debug("(*layerUploadController).Finish")
+func (lw *layerWriter) Finish(digest digest.Digest) (distribution.Layer, error) {
+	ctxu.GetLogger(lw.layerStore.repository.ctx).Debug("(*layerWriter).Finish")
 
-	err := luc.bufferedFileWriter.Close()
+	if err := lw.bufferedFileWriter.Close(); err != nil {
+		return nil, err
+	}
+
+	canonical, err := lw.validateLayer(digest)
 	if err != nil {
 		return nil, err
 	}
 
-	canonical, err := luc.validateLayer(digest)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := luc.moveLayer(canonical); err != nil {
+	if err := lw.moveLayer(canonical); err != nil {
 		// TODO(stevvooe): Cleanup?
 		return nil, err
 	}
 
 	// Link the layer blob into the repository.
-	if err := luc.linkLayer(canonical); err != nil {
+	if err := lw.linkLayer(canonical); err != nil {
 		return nil, err
 	}
 
-	if err := luc.removeResources(); err != nil {
+	if err := lw.removeResources(); err != nil {
 		return nil, err
 	}
 
-	return luc.layerStore.Fetch(canonical)
+	return lw.layerStore.Fetch(canonical)
 }
 
 // Cancel the layer upload process.
-func (luc *layerUploadController) Cancel() error {
-	ctxu.GetLogger(luc.layerStore.repository.ctx).Debug("(*layerUploadController).Cancel")
-	if err := luc.removeResources(); err != nil {
+func (lw *layerWriter) Cancel() error {
+	ctxu.GetLogger(lw.layerStore.repository.ctx).Debug("(*layerWriter).Cancel")
+	if err := lw.removeResources(); err != nil {
 		return err
 	}
 
-	luc.Close()
+	lw.Close()
 	return nil
 }
 
 // validateLayer checks the layer data against the digest, returning an error
 // if it does not match. The canonical digest is returned.
-func (luc *layerUploadController) validateLayer(dgst digest.Digest) (digest.Digest, error) {
+func (lw *layerWriter) validateLayer(dgst digest.Digest) (digest.Digest, error) {
 	// First, check the incoming tarsum version of the digest.
 	version, err := tarsum.GetVersionFromTarsum(dgst.String())
 	if err != nil {
@@ -111,7 +112,7 @@ func (luc *layerUploadController) validateLayer(dgst digest.Digest) (digest.Dige
 	// then only have to fetch the difference.
 
 	// Read the file from the backend driver and validate it.
-	fr, err := newFileReader(luc.bufferedFileWriter.driver, luc.path)
+	fr, err := newFileReader(lw.bufferedFileWriter.driver, lw.path)
 	if err != nil {
 		return "", err
 	}
@@ -140,8 +141,8 @@ func (luc *layerUploadController) validateLayer(dgst digest.Digest) (digest.Dige
 // moveLayer moves the data into its final, hash-qualified destination,
 // identified by dgst. The layer should be validated before commencing the
 // move.
-func (luc *layerUploadController) moveLayer(dgst digest.Digest) error {
-	blobPath, err := luc.layerStore.repository.registry.pm.path(blobDataPathSpec{
+func (lw *layerWriter) moveLayer(dgst digest.Digest) error {
+	blobPath, err := lw.layerStore.repository.registry.pm.path(blobDataPathSpec{
 		digest: dgst,
 	})
 
@@ -150,7 +151,7 @@ func (luc *layerUploadController) moveLayer(dgst digest.Digest) error {
 	}
 
 	// Check for existence
-	if _, err := luc.driver.Stat(blobPath); err != nil {
+	if _, err := lw.driver.Stat(blobPath); err != nil {
 		switch err := err.(type) {
 		case storagedriver.PathNotFoundError:
 			break // ensure that it doesn't exist.
@@ -169,7 +170,7 @@ func (luc *layerUploadController) moveLayer(dgst digest.Digest) error {
 	// the size here and write a zero-length file to blobPath if this is the
 	// case. For the most part, this should only ever happen with zero-length
 	// tars.
-	if _, err := luc.driver.Stat(luc.path); err != nil {
+	if _, err := lw.driver.Stat(lw.path); err != nil {
 		switch err := err.(type) {
 		case storagedriver.PathNotFoundError:
 			// HACK(stevvooe): This is slightly dangerous: if we verify above,
@@ -178,26 +179,26 @@ func (luc *layerUploadController) moveLayer(dgst digest.Digest) error {
 			// prevent this horrid thing, we employ the hack of only allowing
 			// to this happen for the zero tarsum.
 			if dgst == digest.DigestTarSumV1EmptyTar {
-				return luc.driver.PutContent(blobPath, []byte{})
+				return lw.driver.PutContent(blobPath, []byte{})
 			}
 
 			// We let this fail during the move below.
 			logrus.
-				WithField("upload.uuid", luc.UUID()).
+				WithField("upload.uuid", lw.UUID()).
 				WithField("digest", dgst).Warnf("attempted to move zero-length content with non-zero digest")
 		default:
 			return err // unrelated error
 		}
 	}
 
-	return luc.driver.Move(luc.path, blobPath)
+	return lw.driver.Move(lw.path, blobPath)
 }
 
 // linkLayer links a valid, written layer blob into the registry under the
 // named repository for the upload controller.
-func (luc *layerUploadController) linkLayer(digest digest.Digest) error {
-	layerLinkPath, err := luc.layerStore.repository.registry.pm.path(layerLinkPathSpec{
-		name:   luc.layerStore.repository.Name(),
+func (lw *layerWriter) linkLayer(digest digest.Digest) error {
+	layerLinkPath, err := lw.layerStore.repository.registry.pm.path(layerLinkPathSpec{
+		name:   lw.layerStore.repository.Name(),
 		digest: digest,
 	})
 
@@ -205,16 +206,16 @@ func (luc *layerUploadController) linkLayer(digest digest.Digest) error {
 		return err
 	}
 
-	return luc.layerStore.repository.registry.driver.PutContent(layerLinkPath, []byte(digest))
+	return lw.layerStore.repository.registry.driver.PutContent(layerLinkPath, []byte(digest))
 }
 
 // removeResources should clean up all resources associated with the upload
 // instance. An error will be returned if the clean up cannot proceed. If the
 // resources are already not present, no error will be returned.
-func (luc *layerUploadController) removeResources() error {
-	dataPath, err := luc.layerStore.repository.registry.pm.path(uploadDataPathSpec{
-		name: luc.layerStore.repository.Name(),
-		uuid: luc.uuid,
+func (lw *layerWriter) removeResources() error {
+	dataPath, err := lw.layerStore.repository.registry.pm.path(uploadDataPathSpec{
+		name: lw.layerStore.repository.Name(),
+		uuid: lw.uuid,
 	})
 
 	if err != nil {
@@ -225,7 +226,7 @@ func (luc *layerUploadController) removeResources() error {
 	// upload related files.
 	dirPath := path.Dir(dataPath)
 
-	if err := luc.driver.Delete(dirPath); err != nil {
+	if err := lw.driver.Delete(dirPath); err != nil {
 		switch err := err.(type) {
 		case storagedriver.PathNotFoundError:
 			break // already gone!
